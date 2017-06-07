7c902ec4833ed7e294ef088dc9259a7bff092f11
zantiiago0 <zantiiago0@users.noreply.github.com>
2017-06-07 13:32:00 +0200

    + Obtain article body within the JSON response. + Update query endpoint URL. + Use of project API Key. + Include a progress bar for retrieval process. + Use of Named Entity Recognition for query (Hardcoded query)

--- IPGH.py
+++ IPGH.py
@@ -6,54 +6,85 @@ This example deals with returning content of section.
 #IMPORTS
 ###################################################################################################
 #import json
-import time
 import os
 
 #MongoDB
 from   pymongo        import MongoClient
 from   pymongo.errors import DuplicateKeyError
 
+# Natural Language Toolkit
+import nltk
+from nltk.tag import StanfordNERTagger
+
 #Extractor
 from dataExtractors.theGuardianExtractor import TheGuardianExtractor
+
+###################################################################################################
+#CONSTANTS
+###################################################################################################
+
 
 ###################################################################################################
 #FUNCTIONS
 ###################################################################################################
 clear = lambda:os.system('clear')
 
+def InformationExtraction(sentence, useStanford=False):
+    """
+    This system takes the raw text of a document as its input, and generates a list of
+    (entity, relation, entity) tuples as its output.
+    """
+    # Load NLTK Data
+    nltkPath = os.path.dirname(os.path.realpath(__file__)) + '/nltk_data'
+    nltk.data.path.append(nltkPath)
+
+    if not useStanford :
+        # Named Entity Recognition
+        ner = nltk.word_tokenize(sentence)
+        ner = nltk.pos_tag(ner)
+        ner = nltk.ne_chunk(ner)
+    else :
+        nerJar   = nltkPath + '/stanford/stanford-ner.jar'
+        nerModel = nltkPath + '/stanford/english.all.3class.distsim.crf.ser.gz'
+        ner      = StanfordNERTagger(nerModel, nerJar).tag(sentence)
+
+    return ner
+
 ###################################################################################################
 #MAIN
 ###################################################################################################
 clear()
-startOne = time.clock()
 
 #Start MongoDB Client (Default Host)
 mongoDB = MongoClient().local
 
 # Get the ArchivedData Collection
-if "ArchivedData" in mongoDB.collection_names():
+if 'ArchivedData' in mongoDB.collection_names():
     archivedCollection = mongoDB.ArchivedData
 else:
-    mongoDB.create_collection("ArchivedData")
+    mongoDB.create_collection('ArchivedData')
     archivedCollection = mongoDB.ArchivedData
-    archivedCollection.create_index([('name', "text")], unique=True)
+    archivedCollection.create_index([('name', 'text')], unique=True)
 
-# Get the search term
-term = input("What are you searching? ")
+# Get the search query
+query = "Storms in Guadalajara between January 2000 and September 2017"#input('What are you searching? ')
+info = InformationExtraction(query)
 
-query = {"q": term}  # q=query parameter/search parameter
+print(info)
 
-arrayBSON = TheGuardianExtractor().getContent(**query)
+event    = info[0][0]
+location = info[2][0][0]
+fromDate = info[5][0]
+toDate   = info[8][0]
+
+arrayBSON = TheGuardianExtractor().getContent(event, location, fromDate, toDate)
 #jsonContent = json.dumps(arrayBSON, ensure_ascii=False)
 
 for bSON in arrayBSON:
     try:
         archivedCollection.insert_one(bSON)
-        print("New: \"{0}\"".format(bSON['name']))
+        print('New: "{0}"'.format(bSON['name']))
     except DuplicateKeyError:
-        print("Duplicated: \"{0}\"".format(bSON['name']))
-
-#Execution Time
-print(time.clock() - startOne)
+        print('Duplicated: "{0}"'.format(bSON['name']))
 
 #END OF FILE
--- dataExtractors/theGuardianExtractor.py
+++ dataExtractors/theGuardianExtractor.py
@@ -1,16 +1,16 @@
 """
 The Guardian data extractor module
+API: b90595fd-be2a-4488-88bd-538a28af1be2
 """
+import time
+
 from   dataExtractors.document import Document
 
 #The Guardian API
-from   theguardian import theguardian_section
 from   theguardian import theguardian_content
 
 #Web Scrapping
-import requests
 from   bs4 import BeautifulSoup
-from   bs4 import SoupStrainer
 
 class TheGuardianExtractor:
     """
@@ -18,43 +18,80 @@ class TheGuardianExtractor:
     """
 
     def __init__(self):
-        self.__contentTAG = "content__article-body from-content-api js-article__body"
+        self.__contentTAG = 'content__article-body from-content-api js-article__body'
+        self.__API        = 'b90595fd-be2a-4488-88bd-538a28af1be2'
+        self.__toolbarIt  = 0
+        self.__timeStamp  = 0
+        self.__toolbarLen = 0
 
-    def getContent(self, **query):
+    def __printProgressBar(self, iteration, total, tStamp, prefix='', decimals=1, length=50, fill='█'):
+        """
+        Call in a loop to create terminal progress bar
+
+        :Parameters:
+        - `iteration`: Current iteration (Int)
+        - `total`: Total iterations (Int)
+        - `tStamp`: Start time (Int)
+        - `prefix` (optional): Prefix string (Str)
+        - `suffix` (optional): Suffix string (Str)
+        - `decimals` (optional): Positive number of decimals in percent complete (Int)
+        - `length` (optional): Character length of bar (Int)
+        - `fill` (optional): Bar fill character (Str)
+
+        :Returns:
+        """
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        progress = fill * filledLength + '-' * (length - filledLength)
+        timeStamp = time.clock() - tStamp
+        print('\r%s |%s| %s%% - Time: %.3f Sec' % (prefix, progress, percent, timeStamp), end = '\r')
+        # Print New Line on Complete
+        if iteration == total:
+            print()
+
+    def getContent(self, event, location, fromDate, toDate):
         """
         Returns an array of Documents obtained by the query to The Guardian.
 
-        Attributes:
-            :param query: Request to be made to The Guardian API
+        :Parameters:
+        - `event`:
+        - `location`:
+        - `fromDate`:
+        - `toDate`:
         """
-        bSONResult = []
 
-        # Weather apiUrl
-        weatherAPI = "https://content.guardianapis.com/weather"
-
+        bSONResult   = []
+        weatherQuery = {'q':event,
+                        'section':'weather',
+                        'show-fields':'body',
+                        'from-date':'{0}-01-01'.format(fromDate),
+                        'to-date':'{0}-12-31'.format(toDate) }
         # Get weather content
-        content = theguardian_content.Content(api='test', url=weatherAPI, **query)
-
+        content = theguardian_content.Content(api=self.__API, **weatherQuery)
         # Get content response
         response = content.get_content_response()
-
-        for itDoc in response['response']['results']:
-            if itDoc['type'] == "article" :
-                docName    = itDoc['webTitle']
-                docUrl     = itDoc['webUrl']
-                docDate    = itDoc['webPublicationDate']
-                docContent = ""
-                #Extract a Web Page
-                try :
-                    webRequest = requests.get(docUrl)
-                    findTag = SoupStrainer("div", class_=self.__contentTAG)
-                    soup    = BeautifulSoup(webRequest.text, 'lxml', parse_only=findTag)
-                    for article in soup.find_all('p'):
+        # Setup toolbar
+        self.__toolbarIt  = 0
+        self.__toolbarLen = len(content.get_results(response))
+        if self.__toolbarLen > 0:
+            self.__timeStamp = time.clock()
+            self.__printProgressBar(self.__toolbarIt, self.__toolbarLen, self.__timeStamp, prefix='Progress:')
+            # Create documents from database
+            for itDoc in content.get_results(response):
+                if itDoc['type'] == "article" :
+                    docName    = itDoc['webTitle']
+                    docUrl     = itDoc['webUrl']
+                    docDate    = itDoc['webPublicationDate']
+                    docBody    = itDoc['fields']['body']
+                    docContent = ""
+                    #Extract a Web Page
+                    for article in BeautifulSoup(docBody, 'lxml').find_all('p'):
                         docContent = docContent + article.get_text() + "\n"
-
                     bSON = Document(docName, docUrl, docDate, docContent).dictDump()
                     bSONResult.append(bSON)
-                except requests.exceptions.RequestException as e:
-                    print("Error obtaining the webpage" + e)
+                self.__toolbarIt += 1
+                self.__printProgressBar(self.__toolbarIt, self.__toolbarLen, self.__timeStamp, prefix='Retrieving: ')
+        else:
+            print("No results found")
 
         return bSONResult
